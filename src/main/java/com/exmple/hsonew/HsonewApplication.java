e021c4a3ecf286eca8e36298c7ee02ae318243e0
duatnguyen <duatnguyenn23@gmail.com>
2025-07-01 23:52:40 +0700

    base

---
+++ src/main/java/com/exmple/hsonew/HsonewApplication.java
@@ -0,0 +1,13 @@
+package com.exmple.hsonew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HsonewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HsonewApplication.class, args);
+    }
+
+}

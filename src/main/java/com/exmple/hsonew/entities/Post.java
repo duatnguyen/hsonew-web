00f08bafb77ec25c1fd684ff100c9d8056392b0d
duatnguyen <duatnguyenn23@gmail.com>
2025-07-05 17:05:02 +0700

    aaaa

---
+++ src/main/java/com/exmple/hsonew/entities/Post.java
@@ -0,0 +1,4 @@
+package com.exmple.hsonew.entities;
+
+public class Post {
+}
--- src/main/java/com/exmple/hsonew/entities/Ranking.java
+++ src/main/java/com/exmple/hsonew/entities/Ranking.java
@@ -1,4 +1,35 @@
 package com.exmple.hsonew.entities;
 
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Ranking {
+
+    @Id
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+
+    @Column(name =  "level")
+    private int level;
+
+    @Column(name = "point_nap_save")
+    private int pointNapSave;
+
+    @Column(name = "point_san_boss")
+    private int pointSanBoss;
+
 }
